30a70b246570ffc304339600c03e09cbcec0ae05
hyerin <jeonhyelin19951118@gmail.com>
2023-12-03 17:10:10 +0900

    6.1 useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import Button from './Button';
-import styles from './App.module.css';
+import React from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue(prev => prev + 1);
+  console.log('i run all the time');
+
+  // 코드가 한 번만 실행될 수 있도록 도와줌
+  useEffect(() => {
+    console.log('CALL THE API...');
+  }, []);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text={'Continue'} />
-      </div>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me!</button>
+    </div>
   );
 }
 
