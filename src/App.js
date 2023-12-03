c5766df81118190f361af01920879933bc968a58
hyerin <jeonhyelin19951118@gmail.com>
2023-12-03 19:51:06 +0900

    7.0 To Do List part One

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import { useState, useEffect } from 'react';
 
-function Hello() {
-  useEffect(() => {
-    console.log('hi');
-    return () => console.log('bye');
-  }, []);
-  return <h1>Hello</h1>
-}
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing(prev => !prev);
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(todo);
+    if(todo === '') {
+      return;
+    }
+    setTodo('');
+    setTodos(currentArray => [todo, ...currentArray]);
+  }; 
+  console.log(todos);
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My Todos ({todos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" onChange={onChange} value={todo} placeholder="Write your to do..." />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
