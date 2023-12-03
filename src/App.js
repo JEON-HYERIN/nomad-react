c44516b8238fbebd050fc7bafc62a7d4bdd99e71
hyerin <jeonhyelin19951118@gmail.com>
2023-12-03 18:13:55 +0900

    6.4 cleanup

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
-import Button from './Button';
-import React from 'react';
 import { useState, useEffect } from 'react';
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue(prev => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value); 
-
-  // 코드가 한 번만 실행될 수 있도록 도와줌
+function Hello() {
   useEffect(() => {
-    console.log('i run only once');
+    console.log('hi');
+    return () => console.log('bye');
   }, []);
-  useEffect(() => {
-    console.log('i run when "keyword" changes');
-  }, [keyword])
-  useEffect(() => {
-    console.log('i run when "counter" changes');
-  }, [counter])
-  useEffect(() => {
-    console.log('i run when keyword and counter changes');
-  }, [keyword, counter])
+  return <h1>Hello</h1>
+}
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing(prev => !prev);
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me!</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
